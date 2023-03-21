0c80c416659b38faad004f0e8f4fd0fbe191fe91
kmozdzen <kmozdzen2@Onet.pl>
2023-03-21 17:15:22 +0100

    ejs files, routing

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,11 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require("body-parser");
 
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use('/public/', express.static('./public'));
+
+const port = process.env.port || 3000;
+
+//GET
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index', {data: 'data'});
+});
+
+app.get('/login', (req, res) => {
+  res.render('login', {data: 'data'});
+});
+
+app.get('/rejestracja', (req, res) => {
+  res.render('register', {data: 'data'});
+});
+
+app.get('/trening', (req, res) => {
+  res.render('workout', {data: 'data'});
+});
+
+app.get('/cwiczenia', (req, res) => {
+  res.render('exercises', {data: 'data'});
+});
+
+app.get('/profil', (req, res) => {
+  res.render('profile', {data: 'data'});
+});
+
+//POST
+app.post('/login-data', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.post('/register-data', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+//API
+app.get('/cwiczenia/biceps', (req, res) => {
+  
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
